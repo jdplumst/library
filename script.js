774a940497a9e93be7c4cafe0418baf72e36307d
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-12-01 16:50:56 -0500

    Convert plain constructor to class

--- script.js
+++ script.js
@@ -9,14 +9,18 @@ let form = document.querySelector('form');
 let myLibrary = [];
 
 // Book constructor
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        return(title + ' by ' + author + ', ' + pages + ' pages, ' + read + ' yet');
-    }
+class Book {
+
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    };
+    
+    info() {
+        return(this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.read + ' yet');
+    };
 };
 
 // Adds new book to myLibrary and adds it to table
